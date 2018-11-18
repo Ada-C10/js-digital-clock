8764a66f9bf78e9937527f5ef9e0e878d97ea81f
Sigrid Benezra <sdbenezra@gmail.com>
2018-11-17 23:12:22 -0800

    Completed digital clock

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+$(document).ready( () => {
+
+  const timeSet = () => {
+    const time = new Date();
+    const seconds = time.getSeconds();
+    const minutes = time.getMinutes();
+    const hours = time.getHours() - 12;
+
+    let target = $('#clock');
+    target.html(hours).append(':').append(minutes).append(':').append(seconds);
+  }
+
+  let intervalID = setInterval(timeSet, 1000);
+
+  intervalID();
+
+});
