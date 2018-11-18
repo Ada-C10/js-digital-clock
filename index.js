02cd6a9a481bd1449a3e70a5b8b0878aebdcc658
Devin Helmgren <devincollege@gmail.com>
2018-11-18 15:09:46 -0800

    Added the date

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const action = function action(){
+
+
+const updateClock = function updateClock(){
   const justNow = new Date(Date.now());
   const hours = justNow.getHours() < 10 ? "0" + justNow.getHours() : justNow.getHours();
   const mins = justNow.getMinutes() < 10 ? "0" + justNow.getMinutes() : justNow.getMinutes();
   const secs = justNow.getSeconds() < 10 ? "0" + justNow.getSeconds() : justNow.getSeconds();
-  $('#clock').html(`<p>${hours}:${mins}:${secs}</p>`);
+  $('#clock').html(`<p>${justNow.toDateString()} </p>
+  <p>${hours}:${mins}:${secs}</p>`);
 };
 
-const intervalAction = window.setInterval(action, 1000);
+const tickSite = window.setInterval(updateClock, 1000);
 
 $(document).ready(function(){
-  intervalAction;
-
+  tickSite;
 });
