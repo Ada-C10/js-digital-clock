14cf4387849897f5c83fe51015e6a8a2c61015ec
Devin Helmgren <devincollege@gmail.com>
2018-11-18 15:11:45 -0800

    cuter formatting

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-
+const clockElement = $('#clock')
 
 const updateClock = function updateClock(){
   const justNow = new Date(Date.now());
+
   const hours = justNow.getHours() < 10 ? "0" + justNow.getHours() : justNow.getHours();
   const mins = justNow.getMinutes() < 10 ? "0" + justNow.getMinutes() : justNow.getMinutes();
   const secs = justNow.getSeconds() < 10 ? "0" + justNow.getSeconds() : justNow.getSeconds();
-  $('#clock').html(`<p>${justNow.toDateString()} </p>
+
+  clockElement.html(`<p>${justNow.toDateString()} </p>
   <p>${hours}:${mins}:${secs}</p>`);
 };
 
