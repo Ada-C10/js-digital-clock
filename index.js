ef05e37d24c9a8cced3e1db6a46f381706aecddb
Devin Helmgren <devincollege@gmail.com>
2018-11-18 13:47:20 -0800

    boop every second

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const action = function action(){
+  console.log("Boop");
+};
+
+const thingy = window.setInterval(action, 1000);
+
 $(document).ready(function(){
+  thingy;
   $('#clock').append("12:00");
 });
