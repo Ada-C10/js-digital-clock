bc351444499c8e8b2ea2be8523d52ccb4a1eecae
Devin Helmgren <devincollege@gmail.com>
2018-11-18 14:58:42 -0800

    clockin'

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const action = function action(){
-  console.log("Boop");
+  const justNow = new Date(Date.now());
+  const hours = justNow.getHours() < 10 ? "0" + justNow.getHours() : justNow.getHours();
+  const mins = justNow.getMinutes() < 10 ? "0" + justNow.getMinutes() : justNow.getMinutes();
+  const secs = justNow.getSeconds() < 10 ? "0" + justNow.getSeconds() : justNow.getSeconds();
+  $('#clock').html(`<p>${hours}:${mins}:${secs}</p>`);
 };
 
 const intervalAction = window.setInterval(action, 1000);
 
 $(document).ready(function(){
   intervalAction;
-  const justNow = new Date(Date.now());
-  const hours = justNow.getHours() < 10 ? "0" + justNow.getHours() : justNow.getHours();
-  const mins = justNow.getMinutes() < 10 ? "0" + justNow.getMinutes() : justNow.getMinutes();
-  const secs = justNow.getSeconds() < 10 ? "0" + justNow.getSeconds() : justNow.getSeconds();
-  $('#clock').append(`${hours}:${mins}:${secs}`);
+
 });
