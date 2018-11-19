2c8fa80bf31dd4f4675ea43bbd567ce967a1f513
Alice Hsiao <alice.hsiao87@gmail.com>
2018-11-19 13:23:16 -0800

    Added css functionality via jquery events

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
-const month = ["January", "February", "March", "April", "May", "June",
-"July", "August", "September", "October", "November", "December"]
-
 let optionsOne = {
   hour: '2-digit',
   minute: '2-digit',
@@ -48,4 +43,64 @@ $( document ).ready(function() {
   setInterval(() => clock('#taipei'), 1000);
   setInterval(() => clock("#greece"), 1000);
   setInterval(() => clock("#calgary"), 1000);
+
+  $('#btnOne').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("white navy goldenrod");
+    target.toggleClass('tomato');
+  });
+
+  $('#btnTwo').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("white tomato navy");
+    target.toggleClass('goldenrod');
+  });
+
+  $('#btnThree').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("white tomato goldenrod");
+    target.toggleClass('navy');
+  });
+
+  $('#btnFour').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("tomato goldenrod navy");
+    target.toggleClass('white');
+  });
+
+  $('#btnLeft').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("right center");
+    target.toggleClass('left');
+  });
+
+  $('#btnRight').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("left center");
+    target.toggleClass('right');
+  });
+
+  $('#btnCenter').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("left right");
+    target.toggleClass('center');
+  });
+
+  $('#btnAllerta').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("copse crimson");
+    target.toggleClass('allerta');
+  });
+
+  $('#btnCopse').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("allerta crimson");
+    target.toggleClass('copse');
+  });
+
+  $('#btnCrimson').on('click', function() {
+    const target = $('.clockDetails');
+    target.removeClass("allerta copse");
+    target.toggleClass('crimson');
+  });
 })
