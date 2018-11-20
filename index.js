b7f837841cdf9f943b549521d21920f291a178a1
Sigrid Benezra <sdbenezra@gmail.com>
2018-11-19 22:17:39 -0800

    Correct time display. Add color change.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const setupButtons = () => {
+  $('button').click(function() {
+    const colorClass = $(this).html();
+    console.log(`You clicked on the ${colorClass} button`);
+    $('#clock').removeClass();
+    $('#clock').addClass(colorClass);
+  })
+}
+
 $(document).ready( () => {
 
   const timeSet = () => {
-    const time = new Date();
-    const seconds = time.getSeconds();
-    const minutes = time.getMinutes();
-    const hours = time.getHours();
+    const time = new Date(Date.now());
+    const seconds = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
+    const minutes = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
+    const hours = time.getHours() > 12 ? time.getHours() - 12 : time.getHours();
 
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -15,11 +24,17 @@ $(document).ready( () => {
     date.html(time.getMonth() + 1).append('/').append(time.getDate()).append('/').append(time.getFullYear());
 
     const clock = $('#clock');
-    clock.html(hours).append(':').append(minutes).append(':').append(seconds);
+    // clock.html(hours).append(':').append(minutes).append(':').append(seconds);
+    clock.html(`
+      <p>${hours}:${minutes}:${seconds}</p>
+      `);
   }
 
-  let intervalID = setInterval(timeSet, 1000);
+  // let intervalID = setInterval(timeSet, 1000);
+  //
+  // intervalID;
 
-  intervalID;
+  window.setInterval(timeSet, 1000);
+  setupButtons();
 
 });
